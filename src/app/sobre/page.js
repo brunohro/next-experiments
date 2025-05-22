1c74176c76c2bb457019bbbbd6904dca4c0b7ee1
brunohro <brunoh.rodrigues27@gmail.com>
2025-05-22 19:20:05 -0300

    Add navegação entre páginas & rotas dinâmicas

--- src/app/sobre/page.js
+++ src/app/sobre/page.js
@@ -1,13 +1,13 @@
-import Image from "next/image";
 import Link from "next/link";
+import React from "react";
 
-export default function Home() {
+const SobrePage = () => {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1>página home</h1>
-      <Link href="/sobre">Página sobre</Link>
-      <h1>Acessar Posts</h1>
-      <Link href="/posts">Posts</Link>
+      <h1>Página sobre</h1>
+      <Link href="/">Ir para home</Link>
     </div>
   );
-}
+};
+
+export default SobrePage;
