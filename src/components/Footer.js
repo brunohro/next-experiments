847870560ddc2d55618e13c61c147c4a4dd50806
brunohro <brunoh.rodrigues27@gmail.com>
2025-05-30 20:14:02 -0300

    Trabalhando com layout e componentes

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <p>Footer em forma de componente</p>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <h1>Header em forma de componente</h1>
+    </div>
+  );
+};
+
+export default Header;
