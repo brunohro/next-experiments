a2fd77f576ecf7b3f56c3a8c56d184b8938a8585
brunohro <brunoh.rodrigues27@gmail.com>
2025-06-02 16:41:44 -0300

    Ajustando layout do footer + estilizando a home

--- src/app/page.js
+++ src/app/page.js
@@ -3,17 +3,32 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1>página home</h1>
-      <Link href="/sobre">Página sobre</Link>
-      <h1>Acessar Posts</h1>
-      <Link href="/posts">Posts</Link>
-      <Link href="/exemplo?parametro=abc">Exemplo</Link>
-      {/* Nasted routes - rotas aninhadas */}
-      <Link href="/produtos/categorias/roupas">
-        Ir para a Categoria de Roupas
-      </Link>
-      <Link href="/dashboard"> Ir para dashboard</Link>
+    <div className="bg-zinc-200 min-h-screen px-20">
+      <h1 className="text-2xl font-extrabold  text-black">PÁGINA HOME</h1>
+      {/* <Link href="/sobre">Página sobre</Link> */}
+      <div className="max-w-md bg-red-600 flex flex-col m-auto px-4 justify-center h-full rounded-2xl">
+        <h1 className="text-3xl font-bold my-2">ACESSAR POST</h1>
+        <div className="text-xl font-semibold">
+          <nav className="flex flex-col gap-4">
+            <ul>
+              <li>
+                <Link href="/posts">Posts</Link>
+              </li>
+              <li>
+                <Link href="/exemplo?parametro=abc">Exemplo</Link>
+              </li>
+              <li>
+                <Link href="/produtos/categorias/roupas">
+                  Ir para a Categoria de Roupas
+                </Link>
+              </li>
+              <li>
+                <Link href="/dashboard"> Ir para dashboard</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,10 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div>
-      <p>Footer em forma de componente</p>
+    <div className="bg-zinc-700 flex justify-center mx-0 h-20 items-center border-t-2 border-red-600 ">
+      <div>
+        <h1 className="text-2xl font-extrabold  text-white">FOOTER</h1>
+      </div>
     </div>
   );
 };
