20da912ef461657742301a77e86ec07c7aeaa578
brunohro <brunoh.rodrigues27@gmail.com>
2025-06-02 15:59:54 -0300

    Estilizando header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,12 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div>
-      <h1>Header em forma de componente</h1>
+    <div className="bg-zinc-700 flex justify-center mx-0 h-20 items-center border-b-4 border-red-600">
+      <div>
+        <h1 className="text-2xl font-extrabold  text-white">
+          NEXT EXPERIMENTS
+        </h1>
+      </div>
     </div>
   );
 };
